0f925eec895003db34d55bfc1936926b578b6e46
tgn3000 <tgntanguangning@gmail.com>
2021-10-05 15:15:24 +0900

    use macro to print results

--- bin/examples/example_00.C
+++ bin/examples/example_00.C
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdexcept>
 #include <hobbes/hobbes.H>
+#include "example.H"
 
 int applyDiscreteWeighting(int x, int y) { return x * y; }
 
@@ -8,14 +9,16 @@ int main() {
   hobbes::cc c;
   c.bind("applyDiscreteWeighting", &applyDiscreteWeighting);
   std::string x = "abcdefghijklmnopqrstuvwxyz";
-  std::cout << std::boolalpha
-  << c.compileFn<bool(std::string*)>("x", "x[0:2] == \"ab\"")(&x) << ", "
-  << c.compileFn<bool(std::string*)>("x", "x[2:0] == \"ba\"")(&x) << ", "
-  << c.compileFn<bool(std::string*)>("x", "x[-5:-1] == \"vwxy\"")(&x) << ", "
-  << c.compileFn<bool(std::string*)>("x", "x[-5:] == \"vwxyz\"")(&x) << ", "
-  << c.compileFn<bool(std::string*)>("x", "x[:-10] == \"zyxwvutsrq\"")(&x) << ", "
-  << c.compileFn<bool(std::string*)>("x", "x[0:0] == \"\"")(&x) << ", "
-  << c.compileFn<bool(std::string*)>("x", "length(x[:-10]) == 10L")(&x) << ", "
-  << std::endl;
+  RUN_CODE(
+    std::cout << std::boolalpha
+    << c.compileFn<bool(std::string*)>("x", "x[0:2] == \"ab\"")(&x) << ", "
+    << c.compileFn<bool(std::string*)>("x", "x[2:0] == \"ba\"")(&x) << ", "
+    << c.compileFn<bool(std::string*)>("x", "x[-5:-1] == \"vwxy\"")(&x) << ", "
+    << c.compileFn<bool(std::string*)>("x", "x[-5:] == \"vwxyz\"")(&x) << ", "
+    << c.compileFn<bool(std::string*)>("x", "x[:-10] == \"zyxwvutsrq\"")(&x) << ", "
+    << c.compileFn<bool(std::string*)>("x", "x[0:0] == \"\"")(&x) << ", "
+    << c.compileFn<bool(std::string*)>("x", "length(x[:-10]) == 10L")(&x) << ", "
+    << std::endl;
+  );
   return 0;
 }
